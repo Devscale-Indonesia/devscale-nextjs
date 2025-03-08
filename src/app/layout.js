92267a1f5ff01a4837a925845ea7655de2d6b62b
rezarinaldi <rez4rinaldi@gmail.com>
2025-03-08 15:46:19 +0700

    upload template devscale nextjs 2025

---
+++ src/app/layout.js
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Template Next.js | Devscale ID",
+  description: "Template Project Next.js for Devscale ID",
+  icons: {
+    icon: "/icon.svg",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import { Header } from "@/components/header";
+
+export default function Home() {
+  return (
+    <main className="text-xl font-semibold">
+      <Header />
+      <h2 className="text-2xl">
+        Template Project Next.js Devscale Indonesia 🔥
+      </h2>
+    </main>
+  );
+}
